b55bbf2e41feaa7ea7b7ff5c00900ffe6a9230e3
Matthew <mchung2@cs.washington.edu>
2023-02-02 13:50:42 -0800

    template login

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,25 +1,53 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from "react-native";
 import { URI } from "./src/constants";
 
 export default function App() {
-  const [msg, setMsg] = useState("No message");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  async function getMsg() {
+  async function login() {
     try {
-      const resp = await fetch(URI + "/msg");
-      setMsg(await resp.text());
-    } catch (_) {}
-  }
+      const resp = await fetch(URI + "/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
 
-  useEffect(() => {
-    getMsg();
-  }, []);
+      if (resp.ok) {
+        setError("");
+        // navigate to another page or show success message
+      } else {
+        setError(await resp.text());
+      }
+    } catch (_) {
+      setError("Network error");
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <Text>{msg}</Text>
+      <Text style={styles.error}>{error}</Text>
+      <TextInput
+        style={styles.input}
+        value={username}
+        onChangeText={setUsername}
+        placeholder="Username"
+      />
+      <TextInput
+        style={styles.input}
+        value={password}
+        onChangeText={setPassword}
+        placeholder="Password"
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.button} onPress={login}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -31,5 +59,27 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    padding: 20,
+  },
+  error: {
+    color: "red",
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    width: "100%",
+    borderColor: "#ccc",
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+  },
+  button: {
+    backgroundColor: "#4287f5",
+    padding: 10,
+    width: "100%",
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
   },
 });
