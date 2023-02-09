e884617da39e087dbd6b037052ff5a340345fbaa
Lawrence Qupty <lawrencequp@gmail.com>
2023-02-08 21:44:17 -0800

    Async font loading and some other frontend fixed

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,21 +1,22 @@
-import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import FontLoader from "./src/components/FontLoader";
 import LoginScreen from "./src/screens/LoginScreen";
 import SignUpScreen from "./src/screens/SignUpScreen";
 
 const ScreenStack = createNativeStackNavigator();
 
-const AuthStack = () => {
+const Navigator = () => {
   return (
-    <NavigationContainer>
-      <ScreenStack.Navigator screenOptions={{ headerShown: false }}>
-        <ScreenStack.Screen name="Login" component={LoginScreen} />
-        <ScreenStack.Screen name="SignUp" component={SignUpScreen} />
-      </ScreenStack.Navigator>
-    </NavigationContainer>
+    <FontLoader>
+      <NavigationContainer>
+        <ScreenStack.Navigator screenOptions={{ headerShown: false }}>
+          <ScreenStack.Screen name="Login" component={LoginScreen} />
+          <ScreenStack.Screen name="SignUp" component={SignUpScreen} />
+        </ScreenStack.Navigator>
+      </NavigationContainer>
+    </FontLoader>
   );
 };
 
-export default AuthStack;
+export default Navigator;
