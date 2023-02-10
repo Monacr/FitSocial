e7e9353b541129657c7ca49f35e16c1806dab5d8
antzoom1 <antzoom1@cs.washington.edu>
2023-02-09 22:02:43 -0800

    style

--- frontend/src/screens/SignUpScreen.tsx
+++ frontend/src/screens/SignUpScreen.tsx
@@ -5,7 +5,7 @@ import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import LoginScreen from "./SignUpScreen";
 import { NavigationContainer } from "@react-navigation/native";
-import {FC} from 'react';
+import { FC } from 'react';
 
 // interface SwitchingProps {
 //   navigation: any;
@@ -13,23 +13,68 @@ import {FC} from 'react';
 // }
 
 
-const SignUpScreen = ({navigation}) => {
+const SignUpScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={{ justifyContent: 'center', flex: 1 }}>
-      <View style={{paddingHorizontal: 20}}>
-      <View style={{ alignItems: 'center' }}>
-        {/* <img src={LoginSVG} alt = "Image" height={300} width={300}/> */}
-      </View>
-      <Text
-        style={{
-          fontFamily: 'Roboto-Medium',
-          fontSize: 28,
-          fontWeight: '500',
-          color: '#333',
-          marginBottom: 50,
-        }}>
-        Sign Up
-      </Text>
+      <View style={{ paddingHorizontal: 20 }}>
+        <Text
+          style={{
+            fontFamily: 'Roboto-Medium',
+            fontSize: 28,
+            fontWeight: '500',
+            color: '#333',
+            marginBottom: 50,
+          }}>
+          Sign Up
+        </Text>
+
+
+        <View
+          style={{
+            flexDirection: 'row',
+            borderBottomColor: '#ccc',
+            borderBottomWidth: 1,
+            paddingBottom: 5,
+            marginBottom: 20,
+          }}>
+
+
+          <Ionicons name="ios-lock-closed-outline" size={20} color="#666" style={{
+            marginRight: 5
+          }} />
+          <TextInput placeholder='Username'
+            style={{
+              flex: 1,
+              paddingVertical: 0,
+            }}
+            secureTextEntry={true}
+          />
+        </View>
+
+
+
+
+
+        <View
+          style={{
+            flexDirection: 'row',
+            borderBottomColor: '#ccc',
+            borderBottomWidth: 1,
+            paddingBottom: 5,
+            marginBottom: 20,
+          }}>
+          <MaterialIcons name="alternate-email" size={20} color="#666" style={{
+            marginRight: 5
+          }} />
+          <TextInput placeholder='Email ID'
+            style={{
+              flex: 1,
+              paddingVertical: 0,
+            }}
+            keyboardType='email-address'
+          />
+
+        </View>
 
 
 
@@ -38,204 +83,122 @@ const SignUpScreen = ({navigation}) => {
 
 
 
-
-      <View 
-      style={{
-        flexDirection:'row',
-        borderBottomColor: '#ccc',
-        borderBottomWidth: 1,
-        paddingBottom: 5,
-        marginBottom: 20,
-    }}>
-
-
-      <Ionicons name="ios-lock-closed-outline" size={20} color="#666" style={{marginRight: 5
-      }} />
-      <TextInput placeholder='Full Name' 
-      style={{
-      flex:1,
-      paddingVertical:0,
-      }}
-      secureTextEntry={true}
-
-      />
+        <View
+          style={{
+            flexDirection: 'row',
+            borderBottomColor: '#ccc',
+            borderBottomWidth: 1,
+            paddingBottom: 5,
+            marginBottom: 20,
+          }}>
 
 
-      
-      </View>
+          <Ionicons name="ios-lock-closed-outline" size={20} color="#666" style={{
+            marginRight: 5
+          }} />
+          <TextInput placeholder='Password'
+            style={{
+              flex: 1,
+              paddingVertical: 0,
+            }}
+            secureTextEntry={true}
+
+          />
+        </View>
 
 
+        <View
+          style={{
+            flexDirection: 'row',
+            borderBottomColor: '#ccc',
+            borderBottomWidth: 1,
+            paddingBottom: 5,
+            marginBottom: 20,
+          }}>
 
 
+          <Ionicons name="ios-lock-closed-outline" size={20} color="#666" style={{
+            marginRight: 5
+          }} />
+          <TextInput placeholder='Confirm Password'
+            style={{
+              flex: 1,
+              paddingVertical: 0,
+            }}
+            secureTextEntry={true}
 
-      <View 
-      style={{
-        flexDirection:'row',
-        borderBottomColor: '#ccc',
-        borderBottomWidth: 1,
-        paddingBottom: 5,
-        marginBottom: 20,
-    }}>
-      <MaterialIcons name="alternate-email" size={20} color="#666" style={{marginRight: 5
-      }} />
-      <TextInput placeholder='Email ID' 
-      style={{
-      flex:1,
-      paddingVertical:0,
-      }}
-      keyboardType='email-address'
-      />
-
-      </View>
-
-      
-
-      
+          />
 
 
 
-
-      <View 
-      style={{
-        flexDirection:'row',
-        borderBottomColor: '#ccc',
-        borderBottomWidth: 1,
-        paddingBottom: 5,
-        marginBottom: 20,
-    }}>
-
-
-      <Ionicons name="ios-lock-closed-outline" size={20} color="#666" style={{marginRight: 5
-      }} />
-      <TextInput placeholder='Password' 
-      style={{
-      flex:1,
-      paddingVertical:0,
-      }}
-      secureTextEntry={true}
-
-      />
-
-
-    
-      </View>
-
-
-      <View 
-      style={{
-        flexDirection:'row',
-        borderBottomColor: '#ccc',
-        borderBottomWidth: 1,
-        paddingBottom: 5,
-        marginBottom: 20,
-    }}>
-
-
-      <Ionicons name="ios-lock-closed-outline" size={20} color="#666" style={{marginRight: 5
-      }} />
-      <TextInput placeholder='Confirm Password' 
-      style={{
-      flex:1,
-      paddingVertical:0,
-      }}
-      secureTextEntry={true}
-
-      />
-
-
-      
-      </View>
+        </View>
 
 
 
 
 
-      <View 
-      style={{
-        flexDirection:'row',
-        borderBottomColor: '#ccc',
-        borderBottomWidth: 1,
-        paddingBottom: 5,
-        marginBottom: 20,
-    }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            borderBottomColor: '#ccc',
+            borderBottomWidth: 1,
+            paddingBottom: 5,
+            marginBottom: 20,
+          }}>
 
 
-      <Ionicons name="ios-lock-closed-outline" size={20} color="#666" style={{marginRight: 5
-      }} />
-      <TextInput placeholder='Date of Birth'
-      style={{
-      flex:1,
-      paddingVertical:0,
-      }}
-      secureTextEntry={true}
+          <Ionicons name="ios-lock-closed-outline" size={20} color="#666" style={{
+            marginRight: 5
+          }} />
+          <TextInput placeholder='Date of Birth'
+            style={{
+              flex: 1,
+              paddingVertical: 0,
+            }}
+            secureTextEntry={true}
 
-      />
-
-
-      
-      </View>
+          />
 
 
-      
-      
 
-      <TouchableOpacity onPress={() => {}} style={{backgroundColor:'#4287f5', paddingVertical: 10, alignItems: 'center', borderRadius: 5, marginTop: 20}}>
-        <Text style={{color: '#fff', fontSize: 20}}>Sign Up</Text>
-      </TouchableOpacity>
+        </View>
 
-      <Text style = {{textAlign: 'center', color: '#333', fontSize: 12, marginTop: 20, marginBottom: 20,}}>
-        Or, login with...
-      </Text>
 
-      <View style = {{flexDirection: 'row', justifyContent:'space-between', marginBottom: 30}}>
-        
-        <TouchableOpacity onPress={() => {}} 
-      style={{
-      borderColor: '#ddd',
-      borderWidth:2,
-      borderRadius: 10,
-      paddingVertical: 10,
-      paddingHorizontal: 30,
 
-    }}> 
-        <Ionicons name="logo-google" height={24} width = {24}/>
 
-      </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => {}} 
-      style={{
-      borderColor: '#ddd',
-      borderWidth:2,
-      borderRadius: 10,
-      paddingVertical: 10,
-      paddingHorizontal: 30
-    }}> 
-        <Ionicons name="logo-google" height={24} width = {24}/>
-
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={() => {}} 
-      style={{
-      borderColor: '#ddd',
-      borderWidth:2,
-      borderRadius: 10,
-      paddingVertical: 10,
-      paddingHorizontal: 30
-    }}> 
-        <Ionicons name="logo-google" height={24} width = {24}/>
-
-      </TouchableOpacity>
-      </View>
-
-      <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-      <Text> Already Signed Up?</Text>
-      <TouchableOpacity onPress={() => {navigation.goBack()}}>
-        <Text style={{color: '#4287f5', fontSize: 16, marginLeft: 5}}>Log In</Text>
+        <TouchableOpacity onPress={() => { }} style={{ backgroundColor: '#4287f5', paddingVertical: 10, alignItems: 'center', borderRadius: 5, marginTop: 20 }}>
+          <Text style={{ color: '#fff', fontSize: 20 }}>Sign Up</Text>
         </TouchableOpacity>
-      </View>
+
+
+
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 30 }}>
+
+
+
+
+
+
+        </View>
+
+        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+          <Text> Already Signed Up?</Text>
+          <TouchableOpacity onPress={() => { navigation.goBack() }}>
+            <Text style={{ color: '#4287f5', fontSize: 16, marginLeft: 5 }}>Log In</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
 };
+
+const titleStyle={
+  fontFamily: 'Roboto-Medium',
+  fontSize: 28,
+  fontWeight: '500',
+  color: '#333',
+  marginBottom: 50,
+}
 
 export default SignUpScreen;
