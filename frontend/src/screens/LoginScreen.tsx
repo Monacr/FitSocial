82f597986f275d598fd0195d927a907c2282c564
Lawrence Qupty <lawrencequp@gmail.com>
2023-02-15 20:52:57 -0800

    Login screen done

--- frontend/src/screens/LoginScreen.tsx
+++ frontend/src/screens/LoginScreen.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   Text,
   View,
@@ -6,33 +7,63 @@ import {
   TouchableOpacity,
 } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import { LoginInfo } from "../bindings/LoginInfo";
+import { URI } from "../constants";
+import { interactive } from "../styles/Interactive";
 
 const LoginScreen = ({ navigation }) => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [isValid, setIsValid] = useState(false);
+
+  useEffect(() => {
+    setIsValid(name.length > 0 && password.length > 0);
+  }, [name, password]);
+
+  const loginFailed = () => {
+    setError("Incorrect username or password");
+    setName("");
+    setPassword("");
+  };
+
+  const leavePage = (nextPage: string) => {
+    navigation.navigate(nextPage);
+    setError(null);
+    setName("");
+    setPassword("");
+  };
+  const submit = () => {
+    const data: LoginInfo = { username: name, password };
+
+    fetch(URI + "/login", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (res.ok) {
+          leavePage("HomeStack");
+        } else {
+          loginFailed();
+        }
+      })
+      .catch((_) => loginFailed());
+  };
+
   return (
     <SafeAreaView style={{ justifyContent: "center", flex: 1 }}>
       <View style={{ paddingHorizontal: 20 }}>
         <View style={{ alignItems: "center" }}></View>
-        <Text
-          style={{
-            fontFamily: "Roboto-Medium",
-            fontSize: 28,
-            fontWeight: "500",
-            color: "#333",
-            marginBottom: 10,
-          }}
-        >
-          Log In
-        </Text>
+        <Text style={interactive.title}>Log In</Text>
 
-        <View
-          style={{
-            flexDirection: "row",
-            borderBottomColor: "#ccc",
-            borderBottomWidth: 1,
-            paddingBottom: 5,
-            marginBottom: 10,
-          }}
-        >
+        {error && (
+          <Text style={{ ...interactive.error, marginBottom: 10 }}>
+            {error}
+          </Text>
+        )}
+
+        <View style={interactive.bar}>
           <Ionicons
             name="pencil-outline"
             size={20}
@@ -42,7 +73,9 @@ const LoginScreen = ({ navigation }) => {
             }}
           />
           <TextInput
-            placeholder="Email or username"
+            placeholder="Username"
+            onChangeText={setName}
+            value={name}
             style={{
               flex: 1,
               paddingVertical: 0,
@@ -51,15 +84,7 @@ const LoginScreen = ({ navigation }) => {
           />
         </View>
 
-        <View
-          style={{
-            flexDirection: "row",
-            borderBottomColor: "#ccc",
-            borderBottomWidth: 1,
-            paddingBottom: 5,
-            marginBottom: 10,
-          }}
-        >
+        <View style={interactive.bar}>
           <Ionicons
             name="ios-lock-closed-outline"
             size={20}
@@ -70,6 +95,8 @@ const LoginScreen = ({ navigation }) => {
           />
           <TextInput
             placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
             style={{
               flex: 1,
               paddingVertical: 0,
@@ -84,16 +111,9 @@ const LoginScreen = ({ navigation }) => {
           </TouchableOpacity>
         </View>
         <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("HomeStack");
-          }}
-          style={{
-            backgroundColor: "#4287f5",
-            paddingVertical: 10,
-            alignItems: "center",
-            borderRadius: 5,
-            marginTop: 20,
-          }}
+          onPress={submit}
+          disabled={!isValid}
+          style={{ ...interactive.primaryButton, opacity: isValid ? 1 : 0.7 }}
         >
           <Text style={{ color: "#fff", fontSize: 16 }}>Login</Text>
         </TouchableOpacity>
@@ -103,18 +123,16 @@ const LoginScreen = ({ navigation }) => {
             flexDirection: "row",
             justifyContent: "center",
             alignItems: "center",
-            marginTop: 10,
+            marginTop: 30,
           }}
         >
           <Text>Don't have an account?</Text>
           <TouchableOpacity
             onPress={() => {
-              navigation.navigate("SignUp");
+              leavePage("SignUp");
             }}
           >
-            <Text style={{ color: "#4287f5", fontSize: 16, marginLeft: 5 }}>
-              Sign Up
-            </Text>
+            <Text style={interactive.linkButton}>Sign Up</Text>
           </TouchableOpacity>
         </View>
       </View>
