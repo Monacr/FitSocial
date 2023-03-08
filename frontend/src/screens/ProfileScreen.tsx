4e542783797454f158391dc021c5f8fdb01083d1
Michael Dinh <mdinh253@cs.washington.edu>
2023-03-07 16:54:31 -0800

    profile

--- frontend/src/screens/ProfileScreen.tsx
+++ frontend/src/screens/ProfileScreen.tsx
@@ -1,20 +1,34 @@
 import React, {useState, useContext} from 'react';
-import {
-  View,
-  Text,
-  Platform,
-  StyleSheet,
-  Alert,
-  ActivityIndicator,
-  Button,
-} from 'react-native';
+import {View, StyleSheet, StyleSheetProperties} from 'react-native';
+import {Avatar, Title, Caption, Text, TouchableRipple} from "react-native-paper";
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Share from 'react-native-share';
+import {useAuth} from "../components/AuthProvider";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import {PrimaryBlue} from "../constants";
 
 const ProfileScreen = ({navigation}) => {
+    const {authUser} = useAuth();
     return(
-        <View style={styles.container}>
-            <Text>This is your Profile</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.userInfoSection}>
+                <View style={styles.icon}>
+                    <Ionicons name="person-outline"
+                              size={60}
+                              style={{alignSelf:'center'}}
+                    />
+                </View>
+
+                    <View style={{marginLeft: 20}}>
+                        <Title style={ {
+                            marginTop: 15,
+                            marginBottom: 5,
+                        }}>{authUser}</Title>
+                        <Caption style={styles.caption}>{authUser}@gmail.com</Caption>
+                    </View>
+            </View>
+        </SafeAreaView>
     );
 };
 
@@ -23,7 +37,19 @@ export default ProfileScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }, userInfoSection: {
+        flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
-    },
+        justifyContent: 'center',
+        textAlign: 'center',
+    }, icon: {
+        textAlign: 'center',
+        backgroundColor: '#CFCFCF',
+        borderRadius: 100,
+        height: 75,
+        width: 75
+    }
+
 });
